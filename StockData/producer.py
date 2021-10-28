98e9177ea9bdb3b5bc2bc5b6a666ea9a450b977c
NamrathaNShetty <namratanshetty@gmail.com>
2021-10-28 10:34:28 +0530

    [Namratha] Refactor: Added core configuration to .env file

--- StockData/producer.py
+++ StockData/producer.py
@@ -8,10 +8,11 @@ from json import dumps
 from dotenv import load_dotenv
 load_dotenv('.env')
 
-bootstrap_servers = ['localhost:9092']
-producer = KafkaProducer(bootstrap_servers = bootstrap_servers, value_serializer=lambda K:dumps(K).encode('utf-8'))
+#bootstrap_servers = ['localhost:9092']
+producer = KafkaProducer( value_serializer=lambda K:dumps(K).encode('utf-8'))
 
-key = os.getenv('API_KEY')
+
+key = os.getenv('API_KEY','bootstrap_servers')
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 CSV_URL = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY_EXTENDED&symbol=IBM&interval=15min&slice=year1month1&apikey= key'
